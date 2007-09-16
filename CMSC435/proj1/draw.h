3b1eee62990b1ae5ee9fe4f02a7ae41fc5079d73
James Lee <jlee@thestaticvoid.com>
2007-09-16 21:44:52 +0000

    Final project I think

--- CMSC435/proj1/draw.h
+++ CMSC435/proj1/draw.h
@@ -1,8 +1,16 @@
+/*
+ * draw.h
+ * James Lee <jlee23@umbc.edu>
+ */
+
 #ifndef DRAW_H
 #define DRAW_H
 
 #include <ri.h>
 
 void drawCube(RtPoint from, RtPoint to);
+void drawTable(RtFloat length, RtFloat width, RtFloat height);
+void drawLamp(RtPoint position, RtFloat height);
+void drawPencil();
 
 #endif
--- CMSC435/proj1/proj1.c
+++ CMSC435/proj1/proj1.c
@@ -1,52 +1,82 @@
-#include <ri.h>
+/*
+ * proj1.c
+ * James Lee <jlee23@umbc.edu>
+ *
+ * This file contains the main function which sets up the scene by making the
+ * standard calls to the RI to make a tiff with an oblique viewing angle.
+ * It makes calls to other functions to do the more intensive drawing.
+ *
+ * The scene includes a 60"x30"x30" table, a lamp which produces light using a
+ * pointlight and a spotlight, and a pencil that rests on a piece of paper.
+ *
+ * 1 unit = 1 inch
+ */
+
 #include "draw.h"
 
+#define LAMPPOSX (-12.0)
+#define LAMPPOSY 30.0
+#define LAMPPOSZ 2.0
+#define LAMPHEIGHT 15
+
 int main() {
-	RtPoint floor[4] = {{-1000, 0, -1000}, {-1000, 0, 1000}, {1000, 0, 1000}, {1000, 0, -1000}};
-	RtPoint lfLegFrom = {-29.0, 0.0, -14.0}, lfLegTo = {-28.0, 29.0, -13.0};
-	RtPoint lbLegFrom = {-29.0, 0.0, 14.0}, lbLegTo = {-28.0, 29.0, 13.0};
-	RtPoint rfLegFrom = {29.0, 0.0, -14.0}, rfLegTo = {28, 29.0, -13.0};
-	RtPoint rbLegFrom = {29.0, 0.0, 14.0}, rbLegTo = {28, 29.0, 13.0};
-	RtPoint tableTopFrom = {-30.0, 29.0, -15.0}, tableTopTo = {30.0, 30.0, 15.0};
+	RtFloat fov = 90;
+
 	RtColor brown = {0.5, 0.4, 0.3};
 	RtColor black = {0.1, 0.1, 0.1};
+	RtColor white = {1.0, 1.0, 1.0};
 
-	RtPoint spotlightFrom = {-10.0, 45.0, 12.0};
-	RtPoint spotlightTo = {-9.0, 44.0, 11.5};
+	RtPoint lampPos = {LAMPPOSX, LAMPPOSY, LAMPPOSZ};
+	RtPoint spotlightFrom = {LAMPPOSX + 2, LAMPPOSY + LAMPHEIGHT, LAMPPOSZ};
+	RtPoint spotlightTo = {LAMPPOSX + 3, LAMPPOSY + LAMPHEIGHT - 1, LAMPPOSZ - 0.25};
 	RtFloat pointlightIntensity = 50, spotlightIntensity = 500, ambientlightIntensity = 0.3;
+
+	RtPoint paper[4] = {{-4.25, 0.0, -5.5}, {-4.25, 0.0, 5.5}, {4.25, 0.0, 5.5}, {4.25, 0.0, -5.5}};
 
 	RiBegin("proj1.rib");
 		RiDisplay("proj1.tif", "file", "rgba", RI_NULL);
-		RiFormat(800, 600, 1);
 
-		RiProjection("perspective", RI_NULL);
+		/* I like to see the details, so make a nice large image */
+		RiFormat(1920, 1080, 1);
 
-		RiRotate(30.0, -1.0, 1.0, 0.0);
-		RiTranslate(-24.0, -48.0, 36.0);
+		/* Make the field of view larger for the larger image */
+		RiProjection("perspective", "fov", &fov, RI_NULL);
 
+		/* Act like a four foot tall person looking slightly down and to the left
+		 * at the table */
+		RiRotate(30, -1.0, 1.0, 0.0);
+		RiTranslate(-12.0, -48.0, 18.0);
+
+		/* These lights will look like they are coming from the lamp which is drawn
+		 * later */
 		RiLightSource("pointlight", "intensity", &pointlightIntensity, "from", spotlightFrom, RI_NULL);
 		RiLightSource("spotlight", "intensity", &spotlightIntensity, "from", spotlightFrom, "to", spotlightTo, RI_NULL);
+
+		/* This is barely on just to make things look nicer */
 		RiLightSource("ambientlight", "intensity", &ambientlightIntensity, RI_NULL);
 
 		RiWorldBegin();
-			RiPolygon(4, RI_P, floor, RI_NULL);
-
 			RiColor(brown);
 			RiSurface("matte", RI_NULL);
-			drawCube(lfLegFrom, lfLegTo);
-			drawCube(lbLegFrom, lbLegTo);
-			drawCube(rfLegFrom, rfLegTo);
-			drawCube(rbLegFrom, rbLegTo);
-			drawCube(tableTopFrom, tableTopTo);
+			drawTable(60, 30, 30);
+
+			RiColor(black);
+			RiSurface("plastic", RI_NULL);
+			drawLamp(lampPos, LAMPHEIGHT);
 
 			RiTransformBegin();
-				RiTranslate(-12.0, 48.0, 12.0);
-				RiRotate(100, 1.0, 0, 0);
-				RiRotate(45, 0, 1.0, 0);
+				/* The pencil rests at an angle on top of the paper which will be drawn
+				 * next */
+				RiTranslate(0.0, 30.2, 0.0);
+				RiRotate(-45.0, 0.0, 1.0, 0.0);
+				drawPencil();
+			RiTransformEnd();
 
-				RiColor(black);
-				RiSurface("plastic", RI_NULL);
-				RiParaboloid(4, 0, 6, 360, RI_NULL);
+			RiTransformBegin();
+				/* The paper goes slightly above the table */
+				RiTranslate(0.0, 30.01, 0.0);
+				RiColor(white);
+				RiPolygon(4, RI_P, paper, RI_NULL);
 			RiTransformEnd();
 		RiWorldEnd();
 	RiEnd();

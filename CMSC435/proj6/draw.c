4915d9950ae55c239ce2a2e74ea7ae6d1c7e7a1d
James Lee <jlee@thestaticvoid.com>
2007-12-11 01:04:00 +0000

    Proj6 init

---
+++ CMSC435/proj6/draw.c
@@ -0,0 +1,135 @@
+/*
+ * draw.c
+ * James Lee <jlee23@umbc.edu>
+ *
+ * Contains functions that do the meat of the drawing.
+ */
+
+#include "draw.h"
+
+/* Draw a rectangular prism which spans two given opposite corners */
+void drawCube(RtPoint from, RtPoint to) {
+	RtPoint front[4] = {{from[0], from[1], from[2]}, {from[0], to[1], from[2]}, {to[0], to[1], from[2]}, {to[0], from[1], from[2]}};
+	RtPoint back[4] = {{from[0], from[1], to[2]}, {from[0], to[1], to[2]}, {to[0], to[1], from[2]}, {to[0], from[1], to[2]}};
+	RtPoint left[4] = {{from[0], from[1], from[2]}, {from[0], to[1], from[2]}, {from[0], to[1], to[2]}, {from[0], from[1], to[2]}};
+	RtPoint right[4] = {{to[0], from[1], from[2]}, {to[0], to[1], from[2]}, {to[0], to[1], to[2]}, {to[0], from[1], to[2]}};
+	RtPoint top[4] = {{from[0], to[1], from[2]}, {from[0], to[1], to[2]}, {to[0], to[1], to[2]}, {to[0], to[1], from[2]}};
+	RtPoint bottom[4] = {{from[0], from[1], from[2]}, {from[0], from[1], to[2]}, {to[0], from[1], to[2]}, {to[0], from[1], from[2]}};
+
+	RiPolygon(4, RI_P, (RtPointer) front, RI_NULL);
+	RiPolygon(4, RI_P, (RtPointer) back, RI_NULL);
+	RiPolygon(4, RI_P, (RtPointer) left, RI_NULL);
+	RiPolygon(4, RI_P, (RtPointer) right, RI_NULL);
+	RiPolygon(4, RI_P, (RtPointer) top, RI_NULL);
+	RiPolygon(4, RI_P, (RtPointer) bottom, RI_NULL);
+}
+
+/* Calculates the corners of cubes to draw */
+void drawTable(RtFloat length, RtFloat width, RtFloat height) {
+	if (length < 4 || width < 4 || height < 4)
+		return;
+
+	/* The legs are 1" wide and sit 1" in from the edge of the top */
+	RtPoint lfLegFrom = {-(length / 2 - 1), 0.0, -(width / 2 - 1)}, lfLegTo = {-(length / 2 - 2), (height - 1), -(width / 2 - 2)};
+	RtPoint lbLegFrom = {-(length / 2 - 1), 0.0, (width / 2 - 1)}, lbLegTo = {-(length / 2 - 2), (height - 1), (width / 2 - 2)};
+	RtPoint rfLegFrom = {(length / 2 - 1), 0.0, -(width / 2 - 1)}, rfLegTo = {(length / 2 - 2), (height - 1), -(width / 2 - 2)};
+	RtPoint rbLegFrom = {(length / 2 - 1), 0.0, (width / 2 - 1)}, rbLegTo = {(length / 2 - 2), (height - 1), (width / 2 - 2)};
+	RtPoint tableTopFrom = {-(length / 2), (height - 1), -(width / 2)}, tableTopTo = {(length / 2), height, (width / 2)};
+	
+	drawCube(lfLegFrom, lfLegTo);
+	drawCube(lbLegFrom, lbLegTo);
+	drawCube(rfLegFrom, rfLegTo);
+	drawCube(rbLegFrom, rbLegTo);
+	drawCube(tableTopFrom, tableTopTo);
+}
+
+void drawLamp(RtPoint position, RtFloat height) {
+	/* Draw the base of the lamp */
+	RiTransformBegin();
+		RiTranslate(position[0], position[1] + 1, position[2]);
+		RiRotate(90.0, 1.0, 0.0, 0.0);
+		RiDisk(0.0, 4.0, 360.0, RI_NULL);
+		RiCylinder(4.0, 0.0, 1.0, 360.0, RI_NULL);
+	RiTransformEnd();
+
+	/* Draw the stem of the lamp */
+	RiTransformBegin();
+		RiTranslate(position[0], position[1] + height + 1, position[2]);
+		RiRotate(90.0, 1.0, 0.0, 0.0);
+		RiCylinder(0.5, 0.0, height, 360.0, RI_NULL);
+	RiTransformEnd();
+
+	/* Draw the bell shaped part of the lamp */
+	RiTransformBegin();
+		RiTranslate(position[0], position[1] + height + 3, position[2]);
+		RiRotate(100, 1.0, 0, 0);
+		RiRotate(45, 0, 1.0, 0);
+		RiParaboloid(4, 0, 6, 360, RI_NULL);
+	RiTransformEnd();
+}
+
+void drawPencil() {
+	RtColor pink = {1.0, 0.5, 0.5};
+	RtColor silver = {0.7, 0.7, 0.7};
+	RtColor yellow = {1.0, 0.7, 0.0};
+	RtColor tan = {0.9, 0.8, 0.5};
+	RtColor black = {0.1, 0.1, 0.1};
+
+	RtPoint pencilSide[4] = {{-4.0, 0.1, -0.1}, {-4.0, 0.1, 0.1}, {4.0, 0.1, 0.1}, {4.0, 0.1, -0.1}};
+
+	RiColor(yellow);
+	RiSurface("paintedmatte", RI_NULL);
+
+	/* Draw all six sides of a pencil, each 8.0"x0.2" in size */
+	RiPolygon(4, RI_P, pencilSide, RI_NULL);
+	RiRotate(60.0, 1.0, 0.0, 0.0);
+	RiPolygon(4, RI_P, pencilSide, RI_NULL);
+	RiRotate(60.0, 1.0, 0.0, 0.0);
+	RiPolygon(4, RI_P, pencilSide, RI_NULL);
+	RiRotate(60.0, 1.0, 0.0, 0.0);
+	RiPolygon(4, RI_P, pencilSide, RI_NULL);
+	RiRotate(60.0, 1.0, 0.0, 0.0);
+	RiPolygon(4, RI_P, pencilSide, RI_NULL);
+	RiRotate(60.0, 1.0, 0.0, 0.0);
+	RiPolygon(4, RI_P, pencilSide, RI_NULL);
+	
+	/* Draw the eraser */
+	RiTransformBegin();
+		RiTranslate(4.2, 0.0, 0.0);
+
+		/* Draw the metal part around the eraser */
+		RiTransformBegin();
+			RiTranslate(-0.2, 0.0, 0.0);
+			RiRotate(-90.0, 0.0, 1.0, 0.0);
+
+			RiColor(silver);
+			RiSurface("metal", RI_NULL);
+			RiCylinder(0.15, 0.0, 0.3, 360.0, RI_NULL);
+		RiTransformEnd();
+
+		RiRotate(-90.0, 0.0, 1.0, 0.0);
+
+		RiColor(pink);
+		RiSurface("matte", RI_NULL);
+
+		RiDisk(0.0, 0.15, 360, RI_NULL);
+		RiCylinder(0.15, 0.0, 0.2, 360, RI_NULL);
+	RiTransformEnd();
+
+	RiColor(tan);
+	RiSurface("matte", RI_NULL);
+
+	/* Draw the exposed wood part at the bottom of the pencil */
+	RiTranslate(-4.0, 0.0, 0.0);
+	RiTransformBegin();
+		RiRotate(-90.0, 0.0, 1.0, 0.0);
+		RiCone(1.0, 0.15, 360, RI_NULL);
+	RiTransformEnd();
+
+	/* Draw the lead of the pencil */
+	RiTranslate(-0.7, 0.0, 0.0);
+	RiRotate(-90.0, 0.0, 1.0, 0.0);
+
+	RiColor(black);
+	RiCone(0.3, 0.05, 360, RI_NULL);
+}
---
+++ CMSC435/proj6/proj1.c
@@ -0,0 +1,85 @@
+/*
+ * proj1.c
+ * James Lee <jlee23@umbc.edu>
+ *
+ * This file contains the main function which sets up the scene by making the
+ * standard calls to the RI to make a tiff with an oblique viewing angle.
+ * It makes calls to other functions to do the more intensive drawing.
+ *
+ * The scene includes a 60"x30"x30" table, a lamp which produces light using a
+ * pointlight and a spotlight, and a pencil that rests on a piece of paper.
+ *
+ * 1 unit = 1 inch
+ */
+
+#include "draw.h"
+
+#define LAMPPOSX (-12.0)
+#define LAMPPOSY 30.0
+#define LAMPPOSZ 2.0
+#define LAMPHEIGHT 15
+
+int main() {
+	RtFloat fov = 90;
+
+	RtColor brown = {0.5, 0.4, 0.3};
+	RtColor black = {0.1, 0.1, 0.1};
+	RtColor white = {1.0, 1.0, 1.0};
+
+	RtPoint lampPos = {LAMPPOSX, LAMPPOSY, LAMPPOSZ};
+	RtPoint spotlightFrom = {LAMPPOSX + 2, LAMPPOSY + LAMPHEIGHT, LAMPPOSZ};
+	RtPoint spotlightTo = {LAMPPOSX + 3, LAMPPOSY + LAMPHEIGHT - 1, LAMPPOSZ - 0.25};
+	RtFloat pointlightIntensity = 50, spotlightIntensity = 500, ambientlightIntensity = 0.3;
+
+	RtPoint paper[4] = {{-4.25, 0.0, -5.5}, {-4.25, 0.0, 5.5}, {4.25, 0.0, 5.5}, {4.25, 0.0, -5.5}};
+
+	RiBegin("proj1.rib");
+		RiDisplay("proj1.tif", "file", "rgba", RI_NULL);
+
+		/* I like to see the details, so make a nice large image */
+		RiFormat(1920, 1080, 1);
+
+		/* Make the field of view larger for the larger image */
+		RiProjection("perspective", "fov", &fov, RI_NULL);
+
+		/* Act like a four foot tall person looking slightly down and to the left
+		 * at the table */
+		RiRotate(30, -1.0, 1.0, 0.0);
+		RiTranslate(-12.0, -48.0, 18.0);
+
+		/* These lights will look like they are coming from the lamp which is drawn
+		 * later */
+		RiLightSource("pointlight", "intensity", &pointlightIntensity, "from", spotlightFrom, RI_NULL);
+		RiLightSource("spotlight", "intensity", &spotlightIntensity, "from", spotlightFrom, "to", spotlightTo, RI_NULL);
+
+		/* This is barely on just to make things look nicer */
+		RiLightSource("ambientlight", "intensity", &ambientlightIntensity, RI_NULL);
+
+		RiWorldBegin();
+			RiColor(brown);
+			RiSurface("matte", RI_NULL);
+			drawTable(60, 30, 30);
+
+			RiColor(black);
+			RiSurface("plastic", RI_NULL);
+			drawLamp(lampPos, LAMPHEIGHT);
+
+			RiTransformBegin();
+				/* The pencil rests at an angle on top of the paper which will be drawn
+				 * next */
+				RiTranslate(0.0, 30.2, 0.0);
+				RiRotate(-45.0, 0.0, 1.0, 0.0);
+				drawPencil();
+			RiTransformEnd();
+
+			RiTransformBegin();
+				/* The paper goes slightly above the table */
+				RiTranslate(0.0, 30.01, 0.0);
+				RiColor(white);
+				RiPolygon(4, RI_P, paper, RI_NULL);
+			RiTransformEnd();
+		RiWorldEnd();
+	RiEnd();
+
+	return 0;
+}

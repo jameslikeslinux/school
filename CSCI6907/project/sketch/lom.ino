015dbf3bc663beeeeaab22e41288bc4c990d1365
James Lee <jlee@thestaticvoid.com>
2012-02-26 14:25:03 -0500

    CSCI6907 project checkpoint: max3110 interrupts.

--- CSCI6907/project/sketch/lom.ino
+++ CSCI6907/project/sketch/lom.ino
@@ -23,22 +23,23 @@ const int SLAVE_SELECT = 4;
 #define BAUD_7200 0x5
 #define BAUD_9600 0xb
 #define BAUD_19200 0xa
+#define BAUD_38400 0x9
 #define BAUD_57600 0x2
 #define BAUD_115200 0x1
 
 #define SERIAL_BUFFER_SIZE 64
 
 struct ring_buffer {
-    volatile uint8_t buffer[SERIAL_BUFFER_SIZE];
-    volatile unsigned int head;
-    volatile unsigned int tail;
+    uint8_t buffer[SERIAL_BUFFER_SIZE];
+    unsigned int head;
+    unsigned int tail;
 };
 
 static volatile ring_buffer rxbuffer = { { 0 }, 0, 0 };
 static volatile ring_buffer txbuffer = { { 0 }, 0, 0 };
-static volatile uint16_t configureWord = BAUD_9600 | ENABLE_RX_AVAIL_INT;
+static volatile uint16_t configureWord = BAUD_57600 | ENABLE_RX_AVAIL_INT;
 
-bool enqueue(volatile ring_buffer *buffer, uint8_t b) {
+inline bool enqueue(volatile ring_buffer *buffer, uint8_t b) {
     unsigned int i = (buffer->head + 1) % SERIAL_BUFFER_SIZE;
 
     // if we should be storing the received character into the location
@@ -54,11 +55,16 @@ bool enqueue(volatile ring_buffer *buffer, uint8_t b) {
     return false;
 }
 
-bool isempty(volatile ring_buffer *buffer) {
+inline bool isempty(volatile ring_buffer *buffer) {
     return buffer->head == buffer->tail;
 }
 
-bool dequeue(volatile ring_buffer *buffer, uint8_t *b) {
+inline bool isfull(volatile ring_buffer *buffer) {
+    unsigned int i = (buffer->head + 1) % SERIAL_BUFFER_SIZE;
+    return i == buffer->tail;
+}
+
+inline bool dequeue(volatile ring_buffer *buffer, uint8_t *b) {
     if (isempty(buffer))
         return false;
 
@@ -68,7 +74,7 @@ bool dequeue(volatile ring_buffer *buffer, uint8_t *b) {
 }
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(57600);
 
     pinMode(SLAVE_SELECT, OUTPUT);
     digitalWrite(SLAVE_SELECT, HIGH);
@@ -104,56 +110,163 @@ uint16_t maxconfigure() {
 }
 
 bool maxgetbyte(uint8_t *b) {
-    return dequeue(&rxbuffer, b);
+    bool ret;
+
+    // disable interrupts so my buffers don't get clobbered
+    noInterrupts();
+
+    // if the rxbuffer is empty, it's possible I could have missed an
+    // interrupt, so why not go out and get some data
+    if (isempty(&rxbuffer)) {
+        maxisr();
+    }
+
+    // if rxbuffer was empty and the UART still didn't receive any data, this
+    // dequeue operation will fail, and that's okay...just communicate it to
+    // the user with the return code
+    ret = dequeue(&rxbuffer, b);
+
+    // reenable interrupts
+    interrupts();
+
+    return ret;
 }
 
 bool maxputbyte(uint8_t b) {
-    return enqueue(&txbuffer, b);
-}
-
-void maxflushtx() {
+    // disable interrupts so my buffers don't get clobbered
     noInterrupts();
 
-    uint8_t b;
-    uint16_t readword = maxtransfer(READ_DATA);
-
-    if ((readword >> 15) & 1) {
-        enqueue(&rxbuffer, readword);
+    // if my txbuffer is full I can't add the byte to it, and I have no chance
+    // of it clearing out because the rxbuffer needs to have room to do so
+    // so I'm just going to refuse this byte
+    if (isfull(&rxbuffer) && isfull(&txbuffer)) {
+        // reenable interrupts and return
+        interrupts();
+        return false;
     }
 
-    while (((readword >> 14) & 1) && dequeue(&txbuffer, &b)) {
-        readword = maxtransfer(WRITE_DATA | b);
-        if ((readword >> 15) & 1) {
-            enqueue(&rxbuffer, readword);
-        }
+    // if just the txbuffer is full I might have missed an interrupt
+    // so just try forcing some transfers until there room in the buffer
+    while (!enqueue(&txbuffer, b)) {
+        maxisr();
     }
 
+    // now my txbuffer has data so I want to be interrupted when the UART
+    // is able to accept more transfers
+    configureWord |= ENABLE_TX_BUF_INT;
+    maxconfigure();
+
+    // that said, the interrupt seems to only trigger when the hw transmit
+    // buffer *becomes* empty--if it's already empty when the interrupt
+    // is enabled, it doesn't fire...so I have to manually call the interrupt
+    // or bits can get stuck sometimes
+    //
+    // from the datasheet:
+    //   IRQ is asserted low if TM = 1 and the transmit buffer becomes empty.
+    // XXX: This may not be the most efficient thing
+    maxisr();
+
+    // reenable interrupts
     interrupts();
+   
+    return true;
 }
 
-void maxisr() {
-    uint16_t readword = maxtransfer(READ_DATA);
+/* XXX: Evaluate the need for this function
+bool maxavailable() {
+    bool ret;
 
-    if ((readword >> 15) & 1) {
-        enqueue(&rxbuffer, readword);
+    // disable interrupts so my rxbuffer won't change as I'm checking
+    noInterrupts();
+
+    // if the rxbuffer is empty, it's possible I could have missed an
+    // interrupt, so why not go out and get some data
+    if (isempty(&rxbuffer)) {
+        maxisr();
     }
+
+    ret = !isempty(&rxbuffer);
+
+    // reenable interrupts
+    interrupts();
+
+    return ret;
+}
+*/
+
+void maxisr() {
+    uint8_t b;
+    uint16_t readword;
+
+    do {
+        // if the receive buffer is full, there's nothing else we can do
+        // we can continue to let the hardware FIFO fill and hope the user
+        // clears out the rxbuffer before bytes start needing to be dropped
+        if (isfull(&rxbuffer)) {
+            return;
+        }
+
+        readword = maxtransfer(READ_DATA);
+
+        if ((readword >> 15) & 1) {
+            // we got data from the UART and I know I can enqueue it because
+            // I checked above and nothing between then and now could have
+            // added anything else to the rxbuffer
+            enqueue(&rxbuffer, readword);
+        }
+
+        if (((readword >> 14) & 1) && !isempty(&txbuffer)) {
+            // if the UART is able to accept tx, but the receive buffer is now full
+            // then, again we can't do anything because a receive can come back with
+            // the transmit request and we wouldn't have any place to put it.
+            if (isfull(&rxbuffer)) {
+                return;
+            }
+
+            // this dequeue must return data because I checked above and nothing
+            // else could have modified txbuffer between then and now
+            dequeue(&txbuffer, &b);
+
+            readword = maxtransfer(WRITE_DATA | b);
+            if ((readword >> 15) & 1) {
+                // we got data from the UART and I know I can enqueue it because
+                // I checked above and nothing between then and now could have
+                // added anything else to the rxbuffer
+                enqueue(&rxbuffer, readword);
+            }
+            
+            // if that was the last byte in queue to be transferred
+            // then I don't need to be interrupted about the tx buffer anymore
+            if (isempty(&txbuffer)) {
+                configureWord &= ~ENABLE_TX_BUF_INT;
+                maxconfigure();
+            }
+        }
+
+    // and repeat the above as long as the interrupt condition remains and
+    // I'm able to accept new data
+    } while (digitalRead(INTERRUPT) == LOW);
 }
 
 void loop() {
-    uint8_t b;
+    uint8_t b, c;
 
+    // if there's data in the rxbuffer
     if (maxgetbyte(&b)) {
+        // then write a byte of it out the debug serial port
         Serial.write(b);
     }
 
-    if (Serial.available() > 0) {
+    // if there's data available in the debug serial port
+    while (Serial.available() > 0) {
         b = Serial.read();
-        while (!maxputbyte(b)) {
-            maxflushtx();
-        }
-    }
 
-    if (!isempty(&txbuffer)) {
-        maxflushtx();
+        // then try to write a byte of it out the maxim serial port
+        while (!maxputbyte(b)) {
+            // if I can't (because all the buffers are full), then try a read
+            if (maxgetbyte(&c)) {
+                Serial.write(c);
+            }
+        }
     }
 }
